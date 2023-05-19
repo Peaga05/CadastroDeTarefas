af2cf8ed5ca76056c0a446d18d2e366558473a3d
Paulo Henrique Lucindo Serafim <20211STADS026@cefetes.br>
2023-05-19 14:09:03 -0300

    Alteração no estilo

--- app/screens/Alterar.tsx
+++ app/screens/Alterar.tsx
@@ -1,7 +1,6 @@
-import { collection, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import { View, Button, Text, TextInput } from 'react-native';
-import { firestore } from 'react-native-firebase';
+import { View, Button, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';;
 import { FIRESTORE_DB } from '../../firebaseConfig';
 
 const Alterar = ({navigation,  route }: any) => {
@@ -32,23 +31,54 @@ const Alterar = ({navigation,  route }: any) => {
     const handleUpdateTarefa = async () => {
         const colecao = doc(FIRESTORE_DB, 'Tarefas', id);
         await updateDoc(colecao, tarefa);
+        alert("Dados alterados com sucesso")
         navigation.navigate("Lista");
     }
 
     return (
         <View>
             <TextInput
+                style={styles.input}
                 value={tarefa.title}
                 onChangeText={(t) => handleAtualuzaTexto('title', t)}
             />
             <TextInput
+                style={styles.input}
                 value={tarefa.done}
                 onChangeText={(t) => handleAtualuzaTexto('done', t)}
             />
-            <Button title='Alterar' onPress={handleUpdateTarefa} />
+            <TouchableOpacity style={styles.alterar} onPress={handleUpdateTarefa}>
+                <Text style={styles.textoBotao}>Alterar</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 export default Alterar;
 
+const styles = StyleSheet.create({
+    input:{
+        height: 40,
+        margin: 10,
+        fontSize: 20,
+        borderColor: "black",
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 5,
+    },
+    alterar:{
+        backgroundColor: '#46F',
+        width: 200,
+        height: 40,
+        alignItems: 'center',
+        marginHorizontal: 30,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 5,
+        marginLeft: 10,
+    },
+    textoBotao: {
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
+})
